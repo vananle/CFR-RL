39d84eec90e243862fb8cdd766ae8a343934c80f
anle <vananle1993@gmail.com>
2021-09-02 12:16:11 +0900

    refactoring

--- config.py
+++ config.py
@@ -1,24 +1,25 @@
 class NetworkConfig(object):
-  scale = 100
+    scale = 100
 
-  max_step = 1000 * scale
-  
-  initial_learning_rate = 0.0001
-  learning_rate_decay_rate = 0.96
-  learning_rate_decay_step = 5 * scale
-  moving_average_decay = 0.9999
-  entropy_weight = 0.1
+    max_step = 1000 * scale
 
-  save_step = 10 * scale
-  max_to_keep = 1000
+    initial_learning_rate = 0.0001
+    learning_rate_decay_rate = 0.96
+    learning_rate_decay_step = 5 * scale
+    moving_average_decay = 0.9999
+    entropy_weight = 0.1
 
-  Conv2D_out = 128
-  Dense_out = 128
-  
-  optimizer = 'RMSprop'
-  #optimizer = 'Adam'
-    
-  logit_clipping = 10       #10 or 0, = 0 means logit clipping is disabled
+    save_step = 10 * scale
+    max_to_keep = 1000
+
+    Conv2D_out = 128
+    Dense_out = 128
+
+    # optimizer = 'RMSprop'
+    optimizer = 'Adam'
+
+    logit_clipping = 10  # 10 or 0, = 0 means logit clipping is disabled
+
 
 class Config(NetworkConfig):
   version = 'TE_v2'
@@ -26,8 +27,8 @@ class Config(NetworkConfig):
   project_name = 'CFR-RL'
 
   method = 'actor_critic'
-  #method = 'pure_policy'
-  
+  # method = 'pure_policy'
+
   model_type = 'Conv'
 
   topology_file = 'Abilene'
@@ -36,10 +37,11 @@ class Config(NetworkConfig):
 
   tm_history = 1
 
-  max_moves = 10            #percentage
+  max_moves = 10  # percentage
 
   # For pure policy
-  baseline = 'avg'          #avg, best
+  baseline = 'avg'  # avg, best
+
 
 def get_config(FLAGS):
   config = Config
