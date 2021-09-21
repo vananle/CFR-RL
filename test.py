ab62c5e9904c1214ec75ce0962401b25e0a8f3fb
anle <vananle1993@gmail.com>
2021-09-21 17:10:01 +0900

    calculate rc

--- test.py
+++ test.py
@@ -91,7 +91,6 @@ def sim(config, network, game):
         if tm_idx % 50 == 0 and tm_idx != 0:
             print('t       opt_mlu             norm_mlu              mlu          norm_crit_mlu         crit_mlu     '
                   '       norm_topk_mlu          topk_mlu         norm_ecmp_mlu           ecmp_mlu')
-            break
         state = game.get_state(tm_idx)
         if config.method == 'actor_critic':
             policy = network.actor_predict(np.expand_dims(state, 0)).numpy()[0]
